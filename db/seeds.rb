b989f2f5d09ecf4322416f83d7cae1629c9091bf
seran <7030273+seran@users.noreply.github.com>
2022-01-14 17:35:17 +0100

    ui changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create :name => "User", :email => "user@longhall.com", :password => "123456", :role => 0
+admin = User.create :name => "Admin", :email => "admin@longhall.com", :password => "123456", :role => 0
+user = User.create :name => "User", :email => "user@longhall.com", :password => "123456"
 project = Project.create :title => "Just another project", :description => "Nothing interesting here, don't waste your time by opening this.", :user_id => user.id
 scope = Scope.create :title => "iOS Production", :project_id => project.id, :description => "Again just another dummy app.", :version => 1.5, :user_id => user.id
 issue = Issue.create :title => "Login form has a IDOR!! hooray!!", :scope_id => scope.id, :status => 0, :severity => 2, :description => "Nothing serious, ignore this issue.", :solution => "Waiting for a solution for a fictional issue.", :user_id => user.id
