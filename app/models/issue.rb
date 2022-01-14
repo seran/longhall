070e6e8c3ace91780f1c5e092f783525012fb396
seran <7030273+seran@users.noreply.github.com>
2022-01-14 21:55:12 +0100

    sequence number for issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -13,11 +13,18 @@ class Issue < ApplicationRecord
 	scope :filter_by_status, -> (status) { where status: status }
 	scope :filter_by_severity, -> (severity) { where severity: severity }
 	scope :filter_by_title, -> (title) { where("title like ?", "%#{title}%") }
-	scope :filter_by_scope, -> (scope) { where scope_id: scope }
+	scope :filter_by_scope, -> (scope) { joins(:scope).where('scopes.uuid = ?', scope) }
+
+	def sequence_number
+		return nil unless self.persisted?
+		id_with_zeros = "%04d" % self.id
+		"I#{self.created_at.year}#{self.created_at.month}#{self.created_at.day}#{id_with_zeros}"
+	end
 
 	private
 	  def set_defaults
 			self.status ||= :open
 	    self.uuid = SecureRandom.uuid
-	  end
+		end
+
 end
